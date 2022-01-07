8dc955107e7634c2cec599e7bdc302e722c68a48
Co-7 <colin.v42@protonmail.com>
2022-01-07 21:36:21 +0100

    update

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -6,10 +6,34 @@ function Scene(props) {
   
   return (
     <div className={"project " + props.status}>
-        <span>{props.id}</span>
-        <input type="text" placeholder="Question" />
-        <input checked={checked} type="checkbox" />
-        <input type="text" placeholder="url video" value={props.clip_url} />
+        <span id="identifiant">#{props.id}</span>
+        <label htmlFor="url">URL Youtube</label>
+        <input id="url" type="text" placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" value={props.clip_url} />
+        <label htmlFor="question">Question :</label>
+        <input id="question" type="text" placeholder="Voulez vous decrocher le telephone ?" />
+        <div className="choice">
+          <div>
+            <div>
+              <label htmlFor="choice_1">Choix 1 :</label>
+              <input id="choice_1" className="_1" type="text" placeholder="Oui" />
+            </div>
+            <div>
+              <label htmlFor="choice_2">Choix 2 :</label>
+              <input id="choice_2" className="_2" type="text" placeholder="Non" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label htmlFor="choice_1">Clip après choix 1 :</label>
+              <input className="_1" type="text" placeholder="Clip suivant #" />
+            </div>
+            <div>
+              <label htmlFor="choice_2">Clip après choix 2 :</label>
+              <input className="_2" type="text" placeholder="Clip suivant #" />
+            </div>
+          </div>
+        </div>
+        {/* <input checked={checked} type="checkbox" /> */}
     </div>
   );
 }
