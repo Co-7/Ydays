f1bcca027a612b4502b4dc4c10bb43b16b3093c0
Fayaah <louise.baulan@gmail.com>
2022-02-08 23:39:35 +0100

    (fix) backoffice : fix of architecture and wrong react version

--- src/components/BlockProject.jsx
+++ src/components/BlockProject.jsx
@@ -4,10 +4,18 @@ import "../styles/BlockProject.scss";
 
 function Backoffice(props) {
   return (
-    <Link className="block_project" to={`/project/${props.id}`}>
-      <span className="title">{props.title}</span>
-      <span className="author">{props.author}</span>
-    </Link>
+    <div className="blockp">
+      <Link className="block_project" to={`/project/${props.id}`}>
+        <span className="title">{props.title}</span>
+        <span className="author">{props.author}</span>
+      </Link>
+      <Link className="update_project" to={`/project/update/${props.id}`}>
+        Update
+      </Link>
+      <Link className="delete_project" to={`/project/delete/${props.id}`}>
+        Delete
+      </Link>
+    </div>
   );
 }
 
