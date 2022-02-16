369ec50a87314f0713ddc0b1400c060bc2dcce00
Co-7 <colin.v42@protonmail.com>
2022-02-16 17:44:16 +0100

    info (for delete movie)

--- src/views/back-office/movies/MovieDelete.jsx
+++ src/views/back-office/movies/MovieDelete.jsx
@@ -4,6 +4,8 @@ import http from "../../../utils/http-common";
 function MovieDelete() {
     let { id } = useParams();
 
+    // ! Delete les vidéos du film avant sinon error de foreign key
+
     http.delete(`/movies/${id}`)
         .then((response) => (window.location.href = "/"));
 }
