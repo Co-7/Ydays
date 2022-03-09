a0e14bbea32386783c37d8cd8b80b7f1257650aa
Fayaah <louise.baulan@gmail.com>
2022-03-09 18:13:20 +0100

    feat(login) : authentification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import "../assets/styles/Navbar.scss";
 import Logo from '../assets/images/logo.svg';
+import {Link} from "react-router-dom";
 
-class Navbar extends React.Component {
-  render() {
+function Navbar({logged}) {
+
+    const PublicLinks = () => (
+        <div id={"navbar-links"}>
+            <Link to={"/login"}>
+                <p>Login</p>
+            </Link>
+            <Link to={"/register"}>
+                <p>Register</p>
+            </Link>
+        </div>
+    )
+
+    const PrivateLinks = () => (
+        <div id={"navbar-links"}>
+            <Link to={"/"}>
+                <p>Backoffice</p>
+            </Link>
+        </div>
+    )
+    
     return (
-      <nav id="navbar">
-        <img id="logo" src={Logo} alt="Logo"/>
-        <h1>Mov'interact</h1>
-      </nav>
+        <nav id="navbar">
+            <div id={"navbar-spacer"}>
+
+            </div>
+            <div id={"navbar-logo"}>
+                <img id="logo" src={Logo} alt="Logo"/>
+                <h1>Mov'interact</h1>
+            </div>
+            {logged ?
+                <PrivateLinks />
+                :
+                <PublicLinks />
+            }
+        </nav>
     );
-  }
 }
 
 export default Navbar;
