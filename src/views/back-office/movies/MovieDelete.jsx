36959206dfb40071b2b957fa6b343d874df47bd6
Co-7 <colin.v42@protonmail.com>
2022-03-09 10:46:03 +0100

    MovieDelete fix

--- src/views/back-office/movies/MovieDelete.jsx
+++ src/views/back-office/movies/MovieDelete.jsx
@@ -1,13 +1,28 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import http from "../../../utils/http-common";
 
 function MovieDelete() {
-    let { id } = useParams();
+  let { id } = useParams();
 
-    // ! Delete les vidéos du film avant sinon error de foreign key
+  // ! Delete les vidéos du film avant sinon error de foreign key
 
-    http.delete(`/movies/${id}`)
-        .then((response) => (window.location.href = "/"));
+  http.get(`/movies/${id}`).then(function (movie) {
+    if(movie.data.videos.length === 0)
+      http.delete(`/movies/${id}`).then((response) => (window.location.href = "/"));
+    let nb_video = 0;
+    movie.data.videos.forEach(element => {
+      nb_video++;
+      http.delete(`/videos/` + element.id).then((response) => {
+        if(movie.data.videos.length - nb_video === 0)
+          http.delete(`/movies/${id}`).then((response) => (window.location.href = "/"));
+      });
+    });
+    
+  });
+
+  return(
+    <div>delete</div>
+  )
 }
 
 export default MovieDelete;
