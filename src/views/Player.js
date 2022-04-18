487db6928afcf8d2663c97b5b969a89949be94cb
Gianni <ganni.scantamburlo@ynov.com>
2022-04-19 00:16:50 +0200

    player finished

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,12 +1,8 @@
-import ReactPlayer from 'react-player';
 import "../assets/styles/Player.scss";
 // Thème par défaut. ~960B
 import '@vime/core/themes/default.css' ;
 import {
     Player,
-    Youtube,
-    DefaultUi,
-    Provider,
     Ui,
     ClickToPlay,
     Captions,
@@ -19,35 +15,53 @@ import {
     ControlGroup,
     ScrubberControl,
     PlaybackControl,
-    VolumeControl, TimeProgress, ControlSpacer, CaptionControl, PipControl, SettingsControl, FullscreenControl, Embed
+    VolumeControl,
+    TimeProgress,
+    ControlSpacer,
+    CaptionControl,
+    PipControl,
+    SettingsControl,
+    Dailymotion
 } from '@vime/react';
 import React, {useState, useRef} from 'react';
 
 
 // const Player = () => (
 function PlayerVideo(props) {
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState();
+    const [currentTime, setCurrentTime] = useState(parseFloat(0));
+    const [duration, setDuration] = useState(0);
+    const [ready, setReady] = useState(false);
+    const [videoId, setVideoId] = useState('k3b11PemcuTrmWvYe0q');
+    let delayDisplaying = 20;
+
+    const onReady = (event: CustomEvent<number>) => {
+        console.log("onReady");
+        setDuration(event.srcElement.duration);
+        console.log("duration : " + duration.toString());
+        setReady(true);
+    }
 
     const onTimeUpdate = (event: CustomEvent<number>) => {
-        setCurrentTime(event.detail);
-        console.log("TEST");
+        if (ready) {
+            setCurrentTime(event.detail);
+            if (currentTime >= duration - delayDisplaying) {
+                console.log("Boutons affichés")
+            }
+        }
     };
 
-    function test() {
-        console.log(duration);
+    function changeUrl() {
+        setVideoId(null);
+        setVideoId('x8a4xfu');
     }
 
     return (
         <div id="Player">
             <div style={{height: '100%', margin: 'auto'}}>
-                <div className="choices">
-                    <a href="/player">
-                        <button className="player-choice" style={{right: "40vw", left: 0}}>OUI</button>
-                    </a>
-                    <a>
-                        <button className="player-choice" style={{left: "40vw", right: 0}} onClick={test}>NON</button>
-                    </a>
+                <div className="choices"
+                     style={ready && (currentTime >= duration - delayDisplaying) ? {opacity: 1} : {}}>
+                    <button className="player-choice" style={{right: "40vw", left: 0}} onClick={changeUrl}>OUI</button>
+                    <button className="player-choice" style={{left: "40vw", right: 0}} onClick={changeUrl}>NON</button>
                 </div>
                 {/*<div>
                     <button className="player-choice _right"><span className="text">Oui</span></button>
@@ -56,12 +70,12 @@ function PlayerVideo(props) {
                 <Player
                     autoplay
                     aspectRatio="20:9"
-                    theme="dark"
                     style={{'--vm-player-theme': '#9900bf'}}
                     currentTime={currentTime}
-                    duration={duration}
+                    onVmReady={onReady}
+                    onVmCurrentTimeChange={onTimeUpdate}
                 >
-                    <Youtube videoId="DelpERpyqFc"/>
+                    {/*<Youtube videoId="DelpERpyqFc"/>*/}
 
                     {/*<Embed id="vm-iframe-2" embedSrc="https://www.youtube.com/embed/DelpERpyqFc"*/}
                     {/*       params={{autoplay: 0, muted: 1, controls: 1, rel: 0, height: '92vh'}}*/}
@@ -73,14 +87,13 @@ function PlayerVideo(props) {
                     {/*            src="https://www.youtube-nocookie.com/embed/DelpERpyqFc?enablejsapi=1&amp;cc_lang_pref=en&amp;hl=en&amp;fs=1&amp;controls=0&amp;disablekb=1&amp;iv_load_policy=3&amp;mute=0&amp;playsinline=0&amp;autoplay=0"/>*!/*/}
                     {/*</Embed>*/}
 
-                    {/*<DefaultUi noDblClickFullscreen/>*/}
+                    <Dailymotion videoId={videoId} showVideoInfo={false}/>
                     <Ui>
                         <ClickToPlay/>
                         <Captions/>
                         <Poster/>
                         <Spinner/>
                         <LoadingScreen/>
-                        {/*<DefaultControls/>*/}
                         <Controls video="" pin="bottomRight" fullWidth hideOnMouseLeave>
                             <ControlGroup>
                                 <ScrubberControl/>
@@ -90,13 +103,13 @@ function PlayerVideo(props) {
                                 <VolumeControl/>
                                 <TimeProgress/>
                                 <ControlSpacer/>
-                                <CaptionControl hidden=""/>
-                                <PipControl hidden=""/>
+                                <CaptionControl hidden/>
+                                <PipControl hidden/>
                                 <SettingsControl id="vm-settings-control-1"/>
                             </ControlGroup>
                         </Controls>
                         <DefaultSettings pin="bottomRight"/>
-                        <slot></slot>
+                        <slot/>
                     </Ui>
                 </Player>
             </div>
