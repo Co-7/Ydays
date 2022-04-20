8a638760cee242f57f5a622b26b41ced719f8be2
Gianni <ganni.scantamburlo@ynov.com>
2022-04-20 17:16:57 +0200

    player done

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,5 +1,4 @@
 import "../assets/styles/Player.scss";
-// Thème par défaut. ~960B
 import '@vime/core/themes/default.css' ;
 import {
     Player,
@@ -27,8 +26,10 @@ import {
     Dailymotion
 } from '@vime/react';
 import React, {useState, useRef, useEffect} from 'react';
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import http from "../utils/http-common";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateRight } from '@fortawesome/free-solid-svg-icons';
 
 function PlayerVideo(props) {
     let {id} = useParams();
@@ -42,6 +43,7 @@ function PlayerVideo(props) {
     const [ready, setReady] = useState(false);
     const [urlVideo, setUrlVideo] = useState();
     const [player, setPlayer] = useState(false);
+    const [gameOver, setGameOver] = useState(false);
     let delayDisplaying = 20;
 
     const onReady = (event: CustomEvent<number>) => {
@@ -54,18 +56,15 @@ function PlayerVideo(props) {
     const onTimeUpdate = (event: CustomEvent<number>) => {
         if (ready) {
             setCurrentTime(event.detail);
-            if (currentTime >= duration - delayDisplaying) {
-                console.log("Boutons affichés")
-            }
         }
     };
 
     function changeUrl(e) {
         http.get(`/videos/${e.target.getAttribute('targetid')}`)
-        .then(function (a) {
-            localStorage.setItem('video_current', a.data.id);
-            window.location.reload();
-        })
+            .then(function (a) {
+                localStorage.setItem('video_current', a.data.id);
+                window.location.reload();
+            })
     }
 
     function resetAll(e) {
@@ -74,80 +73,93 @@ function PlayerVideo(props) {
     }
 
     useEffect(() => {
-        if(idVideo !== 'undefined' && idVideo !== null) {
+        if (idVideo !== 'undefined' && idVideo !== null) {
             http.get(`/videos/${idVideo}`)
-            .then(function (a) {
-                setFirstChild(a.data.childId[0]);
-                setFirstChoice(a.data.choices[0]);
-                setSecondChild(a.data.childId[1]);
-                setSecondChoice(a.data.choices[1]);
-                setUrlVideo(a.data.url)
-                setPlayer(true);
-            })
+                .then(function (a) {
+                    if(a.data.choices.length !== 0) {
+                        setFirstChild(a.data.childId[0]);
+                        setFirstChoice(a.data.choices[0]);
+                        setSecondChild(a.data.childId[1]);
+                        setSecondChoice(a.data.choices[1]);
+                    }
+                    else{
+                        setGameOver(true);
+                    }
+                    setUrlVideo(a.data.url)
+                    setPlayer(true);
+                })
         } else {
             http.get(`/movies/${id}`)
-            .then(function (a) {
-                a.data.videos.forEach(b => {
-                    if(b.parent) {
-                        setIdVideo(b.id);
-                        setFirstChild(b.childId[0]);
-                        setFirstChoice(b.choices[0]);
-                        setSecondChild(b.childId[1]);
-                        setSecondChoice(b.choices[1]);
-                        setUrlVideo(b.url);
-                        setPlayer(true);
-                    }
+                .then(function (a) {
+                    a.data.videos.forEach(b => {
+                        if (b.parent) {
+                            if(b.choices.length !== 0) {
+                                setFirstChild(b.childId[0]);
+                                setFirstChoice(b.choices[0]);
+                                setSecondChild(b.childId[1]);
+                                setSecondChoice(b.choices[1]);
+                            }
+                            else{
+                                setGameOver(true);
+                            }
+                            setIdVideo(b.id);
+                            setUrlVideo(b.url);
+                            setPlayer(true);
+                        }
+                    })
                 })
-            })
         }
     }, [id]);
 
     return (
         <div id="Player">
-            <button className="button_reset" onClick={resetAll}>Recommencer du début</button>
+            <button className="button_reset" onClick={resetAll} title="Recommencer le film dès le début">
+                <FontAwesomeIcon className="icon" icon={faRotateRight} color="purple" size="4x" />
+            </button>
             <div style={{height: '100%', margin: 'auto'}}>
-                <div className="choices" style={ready && (currentTime >= duration - delayDisplaying) ? {opacity: 1} : {}}>
-                    <a>
-                        <button onClick={changeUrl} targetid={firstChild} className="player-choice" style={{right: "40vw", left: 0}}>{ firstChoice }</button>
-                    </a>
-                    <a>
-                        <button onClick={changeUrl} targetid={secondChild} className="player-choice" style={{left: "40vw", right: 0}}>{ secondChoice }</button>
-                    </a>
+                <div className="choices"
+                     style={ready && !gameOver && (currentTime >= duration - delayDisplaying) ? {opacity: 1} : {}}>
+                    <button onClick={changeUrl} targetid={firstChild} className="player-choice"
+                            style={{right: "40vw", left: 0}}>{firstChoice}</button>
+                    <button onClick={changeUrl} targetid={secondChild} className="player-choice"
+                            style={{left: "40vw", right: 0}}>{secondChoice}</button>
                 </div>
-                {player && <Player
-                    autoplay
-                    aspectRatio="20:9"
-                    style={{'--vm-player-theme': '#9900bf'}}
-                    currentTime={currentTime}
-                    onVmReady={onReady}
-                    onVmCurrentTimeChange={onTimeUpdate}
-                    className='test_player'
-                >
-                    <Dailymotion videoId={urlVideo} showVideoInfo={false}/>
-                    <Ui>
-                        <ClickToPlay/>
-                        <Captions/>
-                        <Poster/>
-                        <Spinner/>
-                        <LoadingScreen/>
-                        <Controls video="" pin="bottomRight" fullWidth hideOnMouseLeave>
-                            <ControlGroup>
-                                <ScrubberControl/>
-                            </ControlGroup>
-                            <ControlGroup>
-                                <PlaybackControl keys=" "/>
-                                <VolumeControl/>
-                                <TimeProgress/>
-                                <ControlSpacer/>
-                                <CaptionControl hidden/>
-                                <PipControl hidden/>
-                                <SettingsControl id="vm-settings-control-1"/>
-                            </ControlGroup>
-                        </Controls>
-                        <DefaultSettings pin="bottomRight"/>
-                        <slot/>
-                    </Ui>
-                </Player>}
+                {player &&
+                    <Player
+                        autoplay
+                        aspectRatio="19:8"
+                        style={{'--vm-player-theme': '#9900bf'}}
+                        currentTime={currentTime}
+                        onVmReady={onReady}
+                        onVmCurrentTimeChange={onTimeUpdate}
+                        className='test_player'
+                    >
+                        <Dailymotion videoId={urlVideo} showVideoInfo={false}/>
+                        <Ui>
+                            <ClickToPlay/>
+                            <Captions/>
+                            <Poster/>
+                            <Spinner/>
+                            <LoadingScreen/>
+                            <Controls video="" pin="bottomRight" fullWidth hideOnMouseLeave>
+                                <ControlGroup>
+                                    <ScrubberControl/>
+                                </ControlGroup>
+                                <ControlGroup>
+                                    <PlaybackControl keys=" "/>
+                                    <VolumeControl/>
+                                    <TimeProgress/>
+                                    <ControlSpacer/>
+                                    <CaptionControl hidden/>
+                                    <PipControl hidden/>
+                                    <SettingsControl id="vm-settings-control-1"/>
+                                </ControlGroup>
+                            </Controls>
+                            <DefaultSettings pin="bottomRight"/>
+                            <slot/>
+                        </Ui>
+                    </Player>
+                }
             </div>
         </div>
     );
